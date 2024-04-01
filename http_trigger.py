f0ff6b3a81ab8fd28eaae5c235841132d14a0a65
mauricio2376 <165286739+mauricio2376@users.noreply.github.com>
2024-04-01 16:10:28 -0300

    http_trigger.py

--- http_trigger.py
+++ http_trigger.py
@@ -1,19 +1,54 @@
-import os
-import json
-import azure.functions as func
 import logging
+import azure.functions as func
+import json
+from datetime import datetime
+
+
+# Obter data e hora atual
+now = datetime.now()
+
+# Extrair informações
+year = now.strftime('%Y')
+month = now.strftime('%m')
+day = now.strftime('%d')
+
+# Inicializa uma lista para armazenar os registros
+records = []
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
+@app.function_name(name="http_trigger")
+
 @app.route(route="http_trigger")
-def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+
+@app.blob_output(arg_name="outputblob",
+                path=f"fsbdntest/{year}/{month}/{day}/Wiki-{year}-{month}-{day}.json",
+                connection="AzureWebJobsStorage")
+
+
+
+def http_trigger(req: func.HttpRequest, outputblob: func.Out[str]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+
+    blob_service = "https://strfuncappbdn.blob.core.windows.net/"
+    path=f"fsbdntest/{year}/{month}/{day}/Wiki-{year}-{month}-{day}.json"
 
     req_body = req.get_json()
 
     if 'pages' in req_body and req_body['pages'][0]['title']:
-        logging.info(req_body)
-        return func.HttpResponse("Page is " + req_body['pages'][0]['title'] + ", Action is " + req_body['pages'][0]['action'])
+        
+        # Adiciona o registro ao final da lista
+        records.append(req_body)
 
+        # Converte a lista de registros para JSON
+        output_json = json.dumps(records)
+
+        # Grava o JSON de saída no blob
+        outputblob.set(output_json)
+
+        #return func.HttpResponse("Page is " + req_body['pages'][0]['title'] + ", Action is " + req_body['pages'][0]['action']),
+        return func.HttpResponse(f"Evento capturado com sucesso e gravado em: {blob_service}{path}")
+    
     else:
         return func.HttpResponse("Invalid payload for Wiki event")
+    
